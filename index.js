c57f100db1b05badbdc7b07f61dd9c7f41e5ace6
Lirihiwa <Lirihiwa@gmail.com>
2026-07-26 14:28:52 +0500

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+const Path = require('./lib/path');
+const {PathElement, ParentPathElement} = require('./lib/pathElement');
+const injectMovement = require('./lib/movement');
+const injectDigging = require('./lib/digging');
+const injectPlacement = require('./lib/placement');
+
+/**
+ * @param {import('mineflayer').Bot} bot
+ */
+function plugin(bot) {
+    if (!bot.cicerone) {
+        bot.cicerone = {};
+        bot.cicerone.settings = {};
+    }
+
+    injectMovement(bot);
+    injectDigging(bot);
+    injectPlacement(bot);
+}
+
+plugin.Path = Path;
+plugin.PathElement = PathElement;
+plugin.ParentPathElement = ParentPathElement;
+
+module.exports = plugin;
