d969701c4700b903012879dc07c432b96d1a4689
Lirihiwa <Lirihiwa@gmail.com>
2026-07-29 02:22:37 +0500

    Create Path.test.js

---
+++ test/Path.test.js
@@ -0,0 +1,24 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const { Vec3 } = require('vec3');
+const plugin = require('../index');
+
+test('Isolated Path construction and fluent API chaining', () => {
+    const actionRegistry = new plugin.ActionRegistry();
+    const structureRegistry = new plugin.PathStructureRegistry();
+
+    // Register mocks actions
+    actionRegistry.register('move', async () => {});
+    actionRegistry.register('dig', async () => {});
+
+    const startPos = new Vec3(0, 0, 0);
+    const path = new plugin.Path(startPos, actionRegistry, structureRegistry);
+
+    assert.strictEqual(path.pathElements.length, 1);
+
+    // Test chaining is works
+    path.add(new Vec3(1, 0, 0), 'move').add(new Vec3(2, 0, 0), 'dig');
+    console.log(path.pathElements);
+    assert.strictEqual(path.pathElements.length, 2);
+    assert.strictEqual(path.pathElements[1].children.length, 1);
+});
