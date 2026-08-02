a536e6d01887772c2f2eddfe70392682d8e0bb63
Lirihiwa <Lirihiwa@gmail.com>
2026-08-02 15:40:25 +0500

    refactor(test): update test

--- test/Path.test.js
+++ test/Path.test.js
@@ -5,19 +5,16 @@ const plugin = require('../index');
 
 test('Isolated Path construction and fluent API chaining', () => {
     const actionRegistry = new plugin.ActionRegistry();
-    const structureRegistry = new plugin.PathStructureRegistry();
 
     // Register mocks actions
     actionRegistry.register('move', async () => {});
     actionRegistry.register('dig', async () => {});
 
-    const startPos = new Vec3(0, 0, 0);
-    const path = new plugin.Path(startPos, actionRegistry, structureRegistry);
+    const path = new plugin.Path(actionRegistry);
 
-    assert.strictEqual(path.pathElements.length, 1);
+    assert.strictEqual(path.elements.length, 0);
 
     // Test chaining is works
     path.add(new Vec3(1, 0, 0), 'move').add(new Vec3(2, 0, 0), 'dig');
-    assert.strictEqual(path.pathElements.length, 2);
-    assert.strictEqual(path.pathElements[1].children.length, 1);
+    assert.strictEqual(path.elements.length, 2);
 });
